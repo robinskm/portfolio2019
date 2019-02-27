947747113ad39058ac00b74d7873b3a2c26347a4
Kristina Robinson <>
2019-02-27 09:13:55 -0500

    hides scrollbar, sets up navigation

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
     $.scrollify({
         section : "section",
-        scrollSpeed: 1000
+        scrollSpeed: 1000,
+        updateHash: false
     });
+    
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+          
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+       
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
 });
